713cd4fb3f41a0b2f0a23b0ade7542f7b59527b8
Renzo Sevilha <sevilharenzo@gmail.com>
2024-04-24 20:26:24 -0300

    feat: implement react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { AirportDocument } from './modules/airport/service';
 import { createAirportService } from './modules/airport/factory';
 import Background from './modules/background/component';
-import CountrySelect from './modules/country-autocomplete/component';
-import Button from '@mui/material/Button';
 import { getCodeByCountryLabel } from './modules/country-autocomplete/countries';
+import Home from './pages/home';
+import Comparison from './pages/comparison';
 
 const DEFAULT_COUNTRY = 'us';
 
 function App() {
   const [airports, setAirports] = useState<AirportDocument[]>([]);
+  const [isLoadingAirports, setIsLoadingAirports] = useState(false);
   const [country, setCountry] = useState(DEFAULT_COUNTRY);
+  const navigate = useNavigate();
 
   const airportsService = createAirportService();
 
@@ -18,41 +21,33 @@ function App() {
     setCountry(getCodeByCountryLabel(countryLabel) || DEFAULT_COUNTRY);
   };
 
-  useEffect(() => {
-    const fetchAirports = async () => {
-      const _airports = await airportsService.getAirportsByCountry(
-        DEFAULT_COUNTRY
-      );
-      setAirports(_airports);
-    };
-
-    fetchAirports();
-  }, []);
-
+  const handleFetchAirports = async () => {
+    setIsLoadingAirports(true);
+    const _airports = await airportsService.getAirportsByCountry(country);
+    setAirports(_airports);
+    setIsLoadingAirports(false);
+    navigate('/comparison');
+  };
+  ('');
   return (
     <Background>
-      <section className="w-full text-center h-full flex flex-col justify-center items-center py-12 px-8">
-        <div className="flex flex-col gap-8 md:gap-14 w-full h-full justify-center items-center">
-          <div className="flex flex-col gap-6">
-            <h1 className="text-slate-100 font-sans text-center text-3xl font-medium sm:text-6xl">
-              Compare Airports <span className="text-slate-300">Distance</span>
-            </h1>
-
-            <p className="text-slate-100 font-light opacity-60 md:text-2xl">
-              Calculate the distance{' '}
-              <span className="text-slate-300">between airports</span>.
-            </p>
-          </div>
-
-          <div>
-            <CountrySelect
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
               handleCountryAutocomplete={handleCountryAutocomplete}
+              handleFetchAirports={handleFetchAirports}
+              isLoadingAirports={isLoadingAirports}
             />
-          </div>
-        </div>
+          }
+        />
 
-        <Button variant="contained">LET'S COMPARE!</Button>
-      </section>
+        <Route
+          path="/comparison"
+          element={<Comparison airports={airports} />}
+        />
+      </Routes>
     </Background>
   );
 }
