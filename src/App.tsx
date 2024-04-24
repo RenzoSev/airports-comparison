a84f3557ec21ac570c9270eaaeafad46b294f8e4
Renzo Sevilha <sevilharenzo@gmail.com>
2024-04-24 17:02:15 -0300

    feat: add airports module

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { AirportDocument } from './modules/airport/service';
+import { createAirportService } from './modules/airport/factory';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [airports, setAirports] = useState<AirportDocument[]>([]);
+  const airportsService = createAirportService();
+  const DEFAULT_COUNTRY = 'us';
+
+  useEffect(() => {
+    const fetchAirports = async () => {
+      const _airports = await airportsService.getAirportsByCountry(
+        DEFAULT_COUNTRY
+      );
+      setAirports(_airports);
+    };
+
+    fetchAirports();
+  }, []);
+
+  console.log({ airports });
 
   return (
     <>
       <div>Hello World</div>
-      <button onClick={() => setCount((prev) => prev + 1)} />
-      <p>{ count }</p>
     </>
   );
 }
